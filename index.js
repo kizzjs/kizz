1e98c17cf9371a93f733b393744c364c39e9e431
Zeno Zeng <zenoofzeng@gmail.com>
2014-07-27 02:47:20 +0800

    init lib/files.js, fixes #43

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var co = require("co"),
     fs = require("co-fs"),
-    fsExtra = require("fs.extra"),
     yaml = require("js-yaml"),
-    mkdirp = require("mkdirp"),
-    walk = require("walk"),
-    path = require("path");
+    mkdirp = require("mkdirp");
 
 var context = {},
     app = new (require("beads"))(context),
@@ -18,7 +15,7 @@ co(function* () {
 
     ////////////////////////////
     //
-    // Init config
+    // Init
     //
     ////////////////////////////
 
@@ -30,7 +27,6 @@ co(function* () {
         console.error("Fail to parse config.yml");
         throw e;
     }
-    context.config = config;
 
     mkdirp.sync("node_modules");
 
@@ -40,16 +36,8 @@ co(function* () {
     //
     ////////////////////////////
 
-    app.use(function *(next) {
-        yield next;
-        context.debug(JSON.stringify(this, null, 4));
-        var cache = {
-            config: context.config,
-            files: context.changedFiles.concat(context.unchangedFiles),
-            time: (new Date()).getTime()
-        };
-        yield fs.writeFile('.cache.json', JSON.stringify(cache, null, 4));
-    });
+    // files cache & walker, the outer control
+    require('./lib/files.js')();
 
     // init plugin manager
     var pluginManager = new PluginManager({registry: config.registry});
@@ -67,91 +55,11 @@ co(function* () {
 
     ////////////////////////////
     //
-    // Check files in content/
-    //
-    ////////////////////////////
-
-    var walkContent = function(cb) {
-        var files = [],
-            walker = walk.walk("content", {followLinks: false});
-        walker.on("file", function(root, stats, next) {
-            var file = {
-                path: root + "/" + stats.name,
-                mtime: stats.mtime,
-                extname: path.extname(stats.name),
-                dirname: root,
-                basename: path.basename(stats.name, path.extname(stats.name))
-            }
-            files.push(file);
-            next();
-        });
-        walker.on("end", function() {
-            cb(null, files);
-        });
-    }
-
-    ////////////////////////////
-    //
-    // Init context
-    //
-    ////////////////////////////
-
-    var cache;
-    try {
-        cache = JSON.parse(fs.readFile('.cache.json', {encoding: 'UTF-8'}));
-    } catch(e) {
-        cache = {config: {plugins: []}};
-    }
-
-    var sourceFiles = yield walkContent;
-
-    var configChanged = (JSON.stringify(cache.config) != JSON.stringify(config));
-    if(!cache.time || configChanged) {
-        cache.time = 0;
-    }
-
-    var pluginsChanged = JSON.stringify(cache.config.plugins) != JSON.stringify(config.plugins);
-    if(pluginsChanged) {
-        yield function(callback) {
-            fsExtra.rmrf('.cache/node_modules', callback);
-        }
-    }
-
-    context.changedFiles = sourceFiles.filter(function(file) {
-        return (new Date(file.mtime)).getTime() > (new Date(cache.time)).getTime();
-    });
-
-    context.removedFiles = [];
-    context.unchangedFiles = [];
-
-    var inArr = function(file, arr) {
-        for (var i = 0; i < arr.length; i++) {
-            if(arr[i].path === file.path) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    if(!cache.files) {
-        cache.files = [];
-    }
-
-    cache.files.forEach(function(file) {
-        if(inArr(file, sourceFiles)) {
-            if(!inArr(file, context.unchangedFiles)) {
-                context.unchangedFiles.push(file);
-            }
-        } else {
-            context.removedFiles.push(file);
-        }
-    });
-
-    ////////////////////////////
-    //
     // Run Middlewares
     //
     ////////////////////////////
+
+    context.config = config;
 
     app.run(function(err) {
         if(err) throw(err);

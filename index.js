286c0e171f0e3c9ff62474bbc1935fc83b238beb
Zeno Zeng <zenoofzeng@gmail.com>
2014-12-31 16:33:20 +0800

    commit.author.avatar = gravatar

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 var gitlog = require('./lib/gitlog');
 
-gitlog(process.cwd(), function(err, data) {
-    if(!err) {
-        console.log(data);
+var gravatar = require('gravatar');
+
+var getAvatar = function(email) {
+    var cache = {};
+    if(!cache.email) {
+        cache.email = gravatar.url(email, {s: '100', r: 'x', d: 'retro'}, true);
     }
+    return cache.email;
+};
+
+gitlog(process.cwd(), function(err, data) {
+    var commits = data.map(function(commit) {
+        commit.author = {
+            name: commit.author.name,
+            avatar: getAvatar(commit.author.email)
+        };
+        return commit;
+    });
+    console.log(commits);
 });
